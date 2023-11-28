a89e0296aa89da6202724b9c51a9ad2fe80f1e8e
Kasper <kasper.mustad.juell@linux.com>
2023-11-28 13:37:46 +0100

    custom rules for autoclosing, not finished

--- lua/plugins/auto-closing.lua
+++ lua/plugins/auto-closing.lua
@@ -1,9 +1,39 @@
 return {
     "steelsojka/pears.nvim",
     config = function ()
-        require("pears").setup()
-        -- require("pears").setup(function (conf)
-        --     conf.preset("tag-matching")
-        -- end)
+        local R = require "pears.rule"
+        require("pears").setup(function (conf)
+            -- conf.preset "tag-matching"
+
+            conf.pair("'", {
+                close = "'",
+                should_expand = R.any_of(
+                    R.end_of_context "[a-zA-z0-9\\s]",
+                    R.not_(R.start_of_context "[a-zA-z0-9]"),
+                    R.not_(R.child_of_node {"string"})
+                )
+            })
+            conf.pair('"', {
+                close = '"',
+                should_expand = R.any_of(
+                    R.end_of_context "[a-zA-z0-9\\s]",
+                    R.not_(R.start_of_context "[a-zA-z0-9]"),
+                    R.not_(R.child_of_node {"string"})
+                )
+            })
+            conf.pair("(", {
+                close = ")",
+                should_expand = R.any_of(
+                    R.end_of_context "[a-zA-z0-9]"
+                    -- R.not_(R.start_of_context "[a-zA-z0-9]")
+                )
+            })
+            conf.pair("{", {
+                close = "}",
+                should_expand = R.any_of(
+                    R.end_of_context "[a-zA-z0-9]"
+                )
+            })
+        end)
     end
 }
