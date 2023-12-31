4c602a755d77d31c92f732a56488eea8244c285d
Kasper <kasper.mustad.juell@linux.com>
2023-12-31 12:37:26 +0100

    removed startscreen

--- lua/plugins/startscreen.lua
+++ lua/plugins/startscreen.lua
@@ -1,38 +1,39 @@
-return {
-    'nvimdev/dashboard-nvim',
-    -- event = 'VimEnter',
-    config = function()
-        require('dashboard').setup {
-            -- config
-            theme = "hyper",
-            config = {
-                header = {
-                    '                                        ▟▙            ',
-                    '                                        ▝▘            ',
-                    '██▃▅▇█▆▖  ▗▟████▙▖   ▄████▄   ██▄  ▄██  ██  ▗▟█▆▄▄▆█▙▖',
-                    '██▛▔ ▝██  ██▄▄▄▄██  ██▛▔▔▜██  ▝██  ██▘  ██  ██▛▜██▛▜██',
-                    '██    ██  ██▀▀▀▀▀▘  ██▖  ▗██   ▜█▙▟█▛   ██  ██  ██  ██',
-                    '██    ██  ▜█▙▄▄▄▟▊  ▀██▙▟██▀   ▝████▘   ██  ██  ██  ██',
-                    '▀▀    ▀▀   ▝▀▀▀▀▀     ▀▀▀▀       ▀▀     ▀▀  ▀▀  ▀▀  ▀▀',
-                    '',
-                },
-                shortcut = {
-                    { desc = '󰊳 Update', group = '@property', action = 'Lazy update', key = 'u' },
-                    {
-                        icon = ' ',
-                        icon_hl = '@variable',
-                        desc = 'Files',
-                        group = 'Label',
-                        action = 'Telescope find_files',
-                        key = 'f',
-                    },
-                },
-                project = { enable = false },
-                packages = { enable = true },
-                mru = { limit = 3, icon = '', label = 'Latest files', cwd_only = true },
-                footer = {},
-            }
-        }
-    end,
-    dependencies = { {'nvim-tree/nvim-web-devicons'}}
-}
+return {}
+-- return {
+--     'nvimdev/dashboard-nvim',
+--     -- event = 'VimEnter',
+--     config = function()
+--         require('dashboard').setup {
+--             -- config
+--             theme = "hyper",
+--             config = {
+--                 header = {
+--                     '                                        ▟▙            ',
+--                     '                                        ▝▘            ',
+--                     '██▃▅▇█▆▖  ▗▟████▙▖   ▄████▄   ██▄  ▄██  ██  ▗▟█▆▄▄▆█▙▖',
+--                     '██▛▔ ▝██  ██▄▄▄▄██  ██▛▔▔▜██  ▝██  ██▘  ██  ██▛▜██▛▜██',
+--                     '██    ██  ██▀▀▀▀▀▘  ██▖  ▗██   ▜█▙▟█▛   ██  ██  ██  ██',
+--                     '██    ██  ▜█▙▄▄▄▟▊  ▀██▙▟██▀   ▝████▘   ██  ██  ██  ██',
+--                     '▀▀    ▀▀   ▝▀▀▀▀▀     ▀▀▀▀       ▀▀     ▀▀  ▀▀  ▀▀  ▀▀',
+--                     '',
+--                 },
+--                 shortcut = {
+--                     { desc = '󰊳 Update', group = '@property', action = 'Lazy update', key = 'u' },
+--                     {
+--                         icon = ' ',
+--                         icon_hl = '@variable',
+--                         desc = 'Files',
+--                         group = 'Label',
+--                         action = 'Telescope find_files',
+--                         key = 'f',
+--                     },
+--                 },
+--                 project = { enable = false },
+--                 packages = { enable = true },
+--                 mru = { limit = 3, icon = '', label = 'Latest files', cwd_only = true },
+--                 footer = {},
+--             }
+--         }
+--     end,
+--     dependencies = { {'nvim-tree/nvim-web-devicons'}}
+-- }
