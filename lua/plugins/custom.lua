af053c6dfc0bb6628f48717becac0d48bc6e19cc
Kasper <kasper.mustad.juell@linux.com>
2024-01-06 00:11:12 +0100

    Added custom keymaps plugin

--- lua/plugins/custom.lua
+++ lua/plugins/custom.lua
@@ -4,5 +4,9 @@ return {
         -- If too slow, only start in config
         dir = "~/Programming/NeovimPlugins/KeymapsPlugin/",
         priority = 100,
+        config = function()
+            require("Keymaps").setup({name = "kasper"})
+            require("helpers.keys").map("n", "<leader>sm", require("Keymaps").open, "Show keymaps")
+        end,
     }
 }
