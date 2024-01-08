00661b65b6690457685ac252f29a4024dfdb3766
Kasper <kasper.mustad.juell@linux.com>
2024-01-08 11:18:57 +0100

    Update plugins

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -4,6 +4,7 @@ return {
     },
     {
         "NvChad/nvim-colorizer.lua", --color
+        commit = "36c610a9717cc9ec426a07c8e6bf3b3abcb139d6",
         config = function ()
         	require("colorizer").setup({'*'})
         end
