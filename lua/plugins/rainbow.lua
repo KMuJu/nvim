7fe14b6492033d451825b387e1add89aa506c77d
Kasper <kasper.mustad.juell@linux.com>
2024-08-05 18:23:34 +0200

    Removed unwanted stuff from config

--- lua/plugins/rainbow.lua
+++ lua/plugins/rainbow.lua
@@ -7,15 +7,12 @@ return {
 		vim.g.rainbow_delimiters = {
 			strategy = {
 				[""] = rainbow_delimiters.strategy["global"],
-				vim = rainbow_delimiters.strategy["local"],
 			},
 			query = {
 				[""] = "rainbow-delimiters",
-				lua = "rainbow-blocks",
 			},
 			priority = {
 				[""] = 110,
-				lua = 210,
 			},
 			highlight = {
 				"RainbowDelimiterRed",
