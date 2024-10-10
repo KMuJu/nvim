b18076bb349117d73af9bea84d0b43218126e258
Kasper <kasper.mustad.juell@linux.com>
2024-10-10 13:40:18 +0200

    feat: remove custom keymaps to decrease startuptime

--- lua/helpers/keys.lua
+++ lua/helpers/keys.lua
@@ -16,27 +16,29 @@ local lsp_map = lm
 M.map = function(mode, lhs, rhs, desc)
 	-- vim.keymap.set(mode, lhs, rhs, { silent = true, desc = desc })
 	-- print(mode, lhs, rhs, desc)
-	if not ok then
-		ok, customMap = pcall(require, "Keymaps.map")
-	end
-	if ok then
-		map = customMap.map
-	else
-		map = m
-	end
+	-- if not ok then
+	-- 	ok, customMap = pcall(require, "Keymaps.map")
+	-- end
+	-- if ok then
+	-- 	map = customMap.map
+	-- else
+	-- 	map = m
+	-- end
+    map = m
 	map(mode, lhs, rhs, desc)
 end
 
 M.lsp_map = function(lhs, rhs, bufnr, desc)
 	-- vim.keymap.set("n", lhs, rhs, { silent = true, buffer = bufnr, desc = desc })
-	if not ok then
-		ok, customMap = pcall(require, "Keymaps.map")
-	end
-	if ok then
-		lsp_map = customMap.lsp_map
-	else
-		lsp_map = lm
-	end
+	-- if not ok then
+	-- 	ok, customMap = pcall(require, "Keymaps.map")
+	-- end
+	-- if ok then
+	-- 	lsp_map = customMap.lsp_map
+	-- else
+	-- 	lsp_map = lm
+	-- end
+    lsp_map = lm
 	lsp_map(lhs, rhs, bufnr, "LSP: " .. desc)
 end
 
