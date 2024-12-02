068e35d7f51b9c88ac976591299dc0334cbd2076
Kasper <kasper.mustad.juell@linux.com>
2024-12-02 16:57:28 +0100

    fix: set buffer for markdown mappings
    Fixed bug where you could use markdown mappings in another filetype
    after opening a markdown file

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -5,4 +5,10 @@ vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 
 -- Markdown show header
-map("n", "<leader>sj", require("custom.pickers.mdheaders").show_headers, "Show markdown headers in telescope")
+vim.keymap.set(
+	"n",
+	"<leader>sj",
+	require("custom.pickers.mdheaders").show_headers,
+	{ buffer = true, desc = "Show markdown headers in telescope" }
+)
+vim.keymap.set("n", "<leader>sh", require("custom.mdOutline").open, { buffer = true, desc = "Open markdown outline" })
