6558c9b1b3a5147d28d606d60fc7d90ef5430b7b
Kasper <kasper.mustad.juell@linux.com>
2024-12-06 20:41:03 +0100

    rewrite: rewrite the split_cmd code

--- lua/custom/mdOutline/util.lua
+++ lua/custom/mdOutline/util.lua
@@ -19,30 +19,25 @@ function M.split(config)
 	vim.api.nvim_set_option_value("swapfile", false, { buf = buf })
 	vim.api.nvim_buf_set_name(buf, config.name)
 
-	local split
-	local size = {}
-	if config.vertical == "auto" then
-		local orig_win = vim.api.nvim_get_current_win()
-		local w = vim.api.nvim_win_get_width(orig_win)
-		local h = vim.api.nvim_win_get_height(orig_win)
-		if w - config.width > (h - config.height) * 2 then
-			split = "aboveleft vsplit"
-			size = { width = config.width }
-		else
-			split = "belowright split"
-			size = { height = config.height }
-		end
-	elseif config.vertical == true then
-		split = "aboveleft vsplit"
+	-- local orig_win = vim.api.nvim_get_current_win()
+	local w = vim.api.nvim_win_get_width(0)
+	local h = vim.api.nvim_win_get_height(0)
+	local split_cmd
+	local size
+	if
+		config.vertical == true
+		or config.vertical == "auto" and config.check_size(w, h, config.width, config.height)
+	then
+		split_cmd = "aboveleft vsplit"
 		size = { width = config.width }
 	else
-		split = "belowright split"
+		split_cmd = "belowright split"
 		size = { height = config.height }
 	end
 
-	vim.cmd(split)
+	vim.cmd(split_cmd)
 	local win = vim.api.nvim_get_current_win()
-	vim.api.nvim_win_set_config(win, size)
+	vim.api.nvim_win_set_config(win, size) -- set the size of the window
 	vim.api.nvim_win_set_buf(win, buf)
 	return buf, win
 end
