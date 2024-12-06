dfa3b75084e740234ba27ce1c447867ab85dc70d
Kasper <kasper.mustad.juell@linux.com>
2024-12-06 13:12:12 +0100

    fix: Moved back to vim.cmd, to allow specifiying side

--- lua/custom/mdOutline/util.lua
+++ lua/custom/mdOutline/util.lua
@@ -10,11 +10,31 @@ function M.split(config)
 	vim.api.nvim_set_option_value("swapfile", false, { buf = buf })
 	vim.api.nvim_buf_set_name(buf, config.name)
 
-	local win = vim.api.nvim_open_win(buf, true, {
-		vertical = config.vertical,
-		split = config.split,
-		width = config.width,
-	})
+	local split
+	local size = {}
+	if config.vertical == "auto" then
+		local orig_win = vim.api.nvim_get_current_win()
+		local w = vim.api.nvim_win_get_width(orig_win)
+		local h = vim.api.nvim_win_get_height(orig_win)
+		if w - config.width > (h - config.height) * 2 then
+			split = "aboveleft vsplit"
+			size = { width = config.width }
+		else
+			split = "belowright split"
+			size = { height = config.height }
+		end
+	elseif config.vertical == true then
+		split = "aboveleft vsplit"
+		size = { width = config.width }
+	else
+		split = "belowright split"
+		size = { height = config.height }
+	end
+
+	vim.cmd(split)
+	local win = vim.api.nvim_get_current_win()
+	vim.api.nvim_win_set_config(win, size)
+	vim.api.nvim_win_set_buf(win, buf)
 	return buf, win
 end
 
