aa9ea78a8f9d51dc51e60a5df50e41adaf62b943
Kasper Mustad Juell <kasper.mustad.juell@gmail.com>
2025-01-18 12:39:26 +0100

    remove custom plugins

--- lua/plugins/custom.lua
+++ lua/plugins/custom.lua
@@ -1,22 +1,22 @@
 local map = require("helpers.keys").map
 return {
-	-- {
-	-- 	-- TODO: Check startup speeds
-	-- 	-- If too slow, only start in config
-	-- 	dir = "~/Programming/NeovimPlugins/KeymapsPlugin/",
-	-- 	priority = 100,
-	-- 	config = function()
-	-- 		require("Keymaps").setup({ name = "kasper" })
-	-- 		require("helpers.keys").map("n", "<leader>sm", require("Keymaps").open, "Show keymaps")
-	-- 	end,
-	-- },
-	{
-		dir = "~/Programming/NeovimPlugins/Filetree.nvim/",
-		priority = 100,
-		config = function()
-			require("Filetree").setup({})
-			map("n", "<leader>po", require("Filetree").open, "Open custom filetree")
-			map("n", "<leader>pp", require("Filetree").togglePreview, "Open custom filetree")
-		end,
-	},
+    -- {
+    -- 	-- TODO: Check startup speeds
+    -- 	-- If too slow, only start in config
+    -- 	dir = "~/Programming/NeovimPlugins/KeymapsPlugin/",
+    -- 	priority = 100,
+    -- 	config = function()
+    -- 		require("Keymaps").setup({ name = "kasper" })
+    -- 		require("helpers.keys").map("n", "<leader>sm", require("Keymaps").open, "Show keymaps")
+    -- 	end,
+    -- },
+    -- {
+    -- 	dir = "~/Programming/NeovimPlugins/Filetree.nvim/",
+    -- 	priority = 100,
+    -- 	config = function()
+    -- 		require("Filetree").setup({})
+    -- 		map("n", "<leader>po", require("Filetree").open, "Open custom filetree")
+    -- 		map("n", "<leader>pp", require("Filetree").togglePreview, "Open custom filetree")
+    -- 	end,
+    -- },
 }
