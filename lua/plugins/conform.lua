2b6b960979efe61c5a8ab977750e36a3a7793bac
Kasper Mustad Juell <kasper.mustad.juell@gmail.com>
2025-02-24 22:12:44 +0100

    format: Formated on save

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,22 +1,23 @@
 return {
-	"stevearc/conform.nvim",
-	-- lazy = true,
-	opts = {
-		notify_on_error = false,
-		format_on_save = {
-			timeout_ms = 500,
-			lsp_fallback = true,
-		},
-		formatters_by_ft = {
-			lua = { "stylua" },
-			-- Conform can also run multiple formatters sequentially
-			python = { "isort", "black" },
-			--
-			-- You can use a sub-list to tell conform to run *until* a formatter
-			-- is found.
-			javascript = { { "prettierd", "prettier" } },
-			-- ocaml = { "ocamlformat" },
-			golang = { "gofmt" },
-		},
-	},
+    "stevearc/conform.nvim",
+    -- lazy = true,
+    opts = {
+        notify_on_error = false,
+        format_on_save = {
+            timeout_ms = 500,
+            lsp_fallback = true,
+        },
+        formatters_by_ft = {
+            c = { "trim_whitespace" },
+            lua = { "stylua" },
+            -- Conform can also run multiple formatters sequentially
+            python = { "isort", "black" },
+            --
+            -- You can use a sub-list to tell conform to run *until* a formatter
+            -- is found.
+            javascript = { { "prettierd", "prettier" } },
+            -- ocaml = { "ocamlformat" },
+            golang = { "gofmt" },
+        },
+    },
 }
--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,49 +1,49 @@
 local map = require("helpers.keys").map
 return {
-	{
-		"lewis6991/gitsigns.nvim",
-		opts = {},
-		config = function()
-			require("gitsigns").setup()
-			map("n", "<leader>gp", "<cmd>Gitsigns preview_hunk<cr>", "Show git changes in hunk")
-		end,
-	},
-	{
-		"akinsho/git-conflict.nvim",
-		commit = "2957f74",
-		config = function()
-			require("git-conflict").setup({
-				default_mappings = true, -- disable buffer local mapping created by this plugin
-				default_commands = true, -- disable commands created by this plugin
-				disable_diagnostics = false, -- This will disable the diagnostics in a buffer whilst it is conflicted
-				list_opener = "copen", -- command or function to open the conflicts list
-				highlights = { -- They must have background color, otherwise the default color will be used
-					incoming = "DiffAdd",
-					current = "DiffText",
-				},
-				debug = true,
-			})
-		end,
-	},
-	-- {
-	-- 	"tpope/vim-fugitive",
-	-- 	enabled = false,
-	-- 	config = function()
-	-- 		map("n", "<leader>ga", "<cmd>Git add %<cr>", "Stage the current file")
-	-- 		map("n", "<leader>gc", "<cmd>Git commit<cr>", "Git commit")
-	-- 		map("n", "<leader>gb", "<cmd>Git blame<cr>", "Show the blame")
-	-- 	end,
-	-- },
-	{
-		"NeogitOrg/neogit",
-		dependencies = {
-			"nvim-lua/plenary.nvim", -- required
-			"sindrets/diffview.nvim", -- optional - Diff integration
+    {
+        "lewis6991/gitsigns.nvim",
+        opts = {},
+        config = function()
+            require("gitsigns").setup()
+            map("n", "<leader>gp", "<cmd>Gitsigns preview_hunk<cr>", "Show git changes in hunk")
+        end,
+    },
+    {
+        "akinsho/git-conflict.nvim",
+        version = "*",
+        config = function()
+            require("git-conflict").setup({
+                default_mappings = true, -- disable buffer local mapping created by this plugin
+                default_commands = true, -- disable commands created by this plugin
+                disable_diagnostics = false, -- This will disable the diagnostics in a buffer whilst it is conflicted
+                list_opener = "copen", -- command or function to open the conflicts list
+                highlights = {   -- They must have background color, otherwise the default color will be used
+                    incoming = "DiffAdd",
+                    current = "DiffText",
+                },
+                debug = true,
+            })
+        end,
+    },
+    -- {
+    -- 	"tpope/vim-fugitive",
+    -- 	enabled = false,
+    -- 	config = function()
+    -- 		map("n", "<leader>ga", "<cmd>Git add %<cr>", "Stage the current file")
+    -- 		map("n", "<leader>gc", "<cmd>Git commit<cr>", "Git commit")
+    -- 		map("n", "<leader>gb", "<cmd>Git blame<cr>", "Show the blame")
+    -- 	end,
+    -- },
+    {
+        "NeogitOrg/neogit",
+        dependencies = {
+            "nvim-lua/plenary.nvim", -- required
+            "sindrets/diffview.nvim", -- optional - Diff integration
 
-			-- Only one of these is needed, not both.
-			"nvim-telescope/telescope.nvim", -- optional
-			"ibhagwan/fzf-lua", -- optional
-		},
-		config = true,
-	},
+            -- Only one of these is needed, not both.
+            "nvim-telescope/telescope.nvim", -- optional
+            "ibhagwan/fzf-lua",     -- optional
+        },
+        config = true,
+    },
 }
