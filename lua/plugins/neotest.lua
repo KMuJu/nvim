d381bcfb4d3f160b046a128b560607cc550423cf
Kasper Mustad Juell <kasper.mustad.juell@gmail.com>
2025-04-24 12:29:06 +0200

    Plugin: Add neotest
    Could not get it to work with java :(

---
+++ lua/plugins/neotest.lua
@@ -0,0 +1,28 @@
+return {
+  {
+    "rcasia/neotest-java",
+    ft = "java",
+    dependencies = {
+      "mfussenegger/nvim-jdtls",
+      "mfussenegger/nvim-dap", -- for the debugger
+      "rcarriga/nvim-dap-ui", -- recommended
+      "theHamsta/nvim-dap-virtual-text", -- recommended
+    },
+  },
+  {
+    "nvim-neotest/neotest",
+    dependencies = {
+      "nvim-neotest/nvim-nio",
+      "nvim-lua/plenary.nvim",
+      "antoinemadec/FixCursorHold.nvim",
+      "nvim-treesitter/nvim-treesitter"
+    },
+    opts = {
+      adapters = {
+        ["neotest-java"] = {
+          -- config here
+        },
+      },
+    },
+  },
+}
