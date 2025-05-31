36671bc155e4f61aa5f4a29e4c82b2b239e07d44
Kasper Mustad Juell <kasper.mustad.juell@gmail.com>
2025-05-31 08:49:58 +0200

    Filetypes: Add more filetypes

--- init.lua
+++ init.lua
@@ -12,5 +12,18 @@ require("autocmd")
 
 require("custom.startscreen").setup()
 
+vim.filetype.add({
+	extension = { rasi = "rasi", rofi = "rasi", wofi = "rasi" },
+	filename = {
+		["vifmrc"] = "vim",
+	},
+	pattern = {
+		[".*/kitty/.+%.conf"] = "kitty",
+		[".*/hypr/.+%.conf"] = "hyprlang",
+		["%.env%.[%w_.-]+"] = "sh",
+	},
+})
+vim.treesitter.language.register("bash", "kitty")
+
 local colorscheme = require("helpers.theme")
 vim.cmd.colorscheme(colorscheme)
