5c2e9ddff063f6dc744fb630f524429712da8ba0
Kasper Mustad Juell <kasper.mustad.juell@gmail.com>
2025-05-31 08:49:00 +0200

    Folds: Use Lsp folds when possible

--- lua/folds.lua
+++ lua/folds.lua
@@ -36,8 +36,19 @@ function _G.fold_text()
 	return string.rep(" ", whitespace) .. line .. middle .. rightstr
 end
 
-vim.opt.foldmethod = "expr"
-vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+vim.o.foldmethod = 'expr'
+-- Default to treesitter folding
+vim.o.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+-- Prefer LSP folding if client supports it
+vim.api.nvim_create_autocmd('LspAttach', {
+    callback = function(args)
+        local client = vim.lsp.get_client_by_id(args.data.client_id)
+        if client and client:supports_method('textDocument/foldingRange') then
+            local win = vim.api.nvim_get_current_win()
+            vim.wo[win][0].foldexpr = 'v:lua.vim.lsp.foldexpr()'
+        end
+    end,
+})
 vim.opt.foldenable = false
 vim.opt.foldlevel = 99
 
