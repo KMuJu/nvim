e075a3a575946a724644ae491d9bf11ba1652b9f
Kasper Mustad Juell <kasper.mustad.juell@gmail.com>
2025-11-05 21:36:16 -0300

    fix(markview): list rendering problem

--- lua/plugins/markview.lua
+++ lua/plugins/markview.lua
@@ -9,32 +9,33 @@ return {
 		-- Or if the parsers are in your $RUNTIMEPATH
 		"nvim-treesitter/nvim-treesitter",
 
-        "nvim-tree/nvim-web-devicons",
-    },
-    config = function()
-        local map = require("helpers.keys").map
-        map("n", "<leader>m", "<cmd>Markview toggle<CR>", "Toggle markdown view")
-        require("markview").setup({
-            markdown = {
-                list_items = {
-                    shift_width = function (buffer, item)
-                        --- Reduces the `indent` by 1 level.
-                        ---
-                        ---         indent                      1
-                        --- ------------------------- = 1 ÷ --------- = new_indent
-                        --- indent * (1 / new_indent)       new_indent
-                        ---
-                        local parent_indnet = math.max(1, item.indent - vim.bo[buffer].shiftwidth);
+		"nvim-tree/nvim-web-devicons",
+	},
+	config = function()
+		local map = require("helpers.keys").map
+		map("n", "<leader>m", "<cmd>Markview toggle<CR>", "Toggle markdown view")
+		require("markview").setup({
+			markdown = {
+				list_items = {
+					shift_width = function(buffer, item)
+						--- Reduces the `indent` by 1 level.
+						---
+						---         indent                      1
+						--- ------------------------- = 1 ÷ --------- = new_indent
+						--- indent * (1 / new_indent)       new_indent
+						---
+						local parent_indnet = math.max(1, item.indent - vim.bo[buffer].shiftwidth)
 
-                        return (item.indent) * (1 / (parent_indnet * 2));
-                    end,
-                    marker_minus = {
-                        add_padding = function (_, item)
-                            return item.indent > 1;
-                        end
-                    }
-                }
-            }
-        })
-    end,
+						return item.indent * (1 / (parent_indnet * 2))
+					end,
+					marker_minus = {
+						add_padding = false,
+					},
+					marker_star = {
+						add_padding = false,
+					},
+				},
+			},
+		})
+	end,
 }
